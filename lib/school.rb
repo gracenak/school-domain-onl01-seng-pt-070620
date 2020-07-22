61413c77842d26db49d12df05e2287a96644499d
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 20:13:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+  
+  def initialize(new)
+    @new = new
+    @roster = {}
+  end
+  
+def add_student(name,grade)
+  if @roster[grade] == nil
+    @grade = grade
+    @roster[@grade] = []
+    @roster[@grade] << name
+  else
+    @roster[grade]<< name
+  end
+end
+
+def grade(grade)
+  return @roster[grade]
+end
+
+end
+
+  
