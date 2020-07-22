bcb308c45eeef870584b820e9e46a7beab094a48
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 20:39:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,32 +8,21 @@ class School
     @roster = {}
   end
   
-  def add_a_student(name, grade)
-    @name = name
-    @grade = grade
-    if @roster[grade]== {}
-      @roster[grade] << name
-  end
   
-#   def initialize(new)
-#     @new = new
-#     @roster = {}
-#   end
-  
-def add_student(name,grade)
-  if @roster[grade] == nil
-    @grade = grade
-    @roster[@grade] = []
-    @roster[@grade] << name
-  else
-    @roster[grade]<< name
+  def add_student(name,grade)
+    if @roster[grade] == nil
+      @grade = grade
+      @roster[@grade] = []
+      @roster[@grade] << name
+    else
+      @roster[grade]<< name
+    end
   end
+
+  def grade(grade)
+    return @roster[grade]
 end
 
-# def grade(grade)
-#   return @roster[grade]
-# end
-
-# end
+end
 
   
